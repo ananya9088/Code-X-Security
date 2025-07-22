27ca52d55aca78cbe2e06c60a1c858862dcd4604
ananya9088 <puja46505@gmail.com>
2025-07-22 09:53:19 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "CodeX"
+NAME = "DARKMI"
 server = "https://discord.com/invite/code-verse"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [1383706658315960330, 1383706658315960330]
-BotName = "CodeX"
-serverLink = "https://discord.com/invite/code-verse"
+OWNER_IDS = [811832394570792971]
+BotName = "DARKNIGHT"
+serverLink = "https://discord.com/invite/DARKNIGHT"
